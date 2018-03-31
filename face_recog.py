00b3684658052fab10efce2256f03db5f1cb3f86
Itachi6912110 <itachi691211011129@gmail.com>
2018-03-31 16:24:34 +0800

    add camera

--- face_recog.py
+++ face_recog.py
@@ -2,7 +2,8 @@ import cognitive_face as CF
 import requests
 from io import BytesIO
 from PIL import Image, ImageDraw
-import matplotlib.pyplot as plt
+import numpy as np
+import cv2
 
 KEY = '81584f93ae644d1497bb4982d5ff6ef9'  # Replace with a valid subscription key (keeping the quotes in place).
 CF.Key.set(KEY)
@@ -32,33 +33,54 @@ def get_face_data(filename):
 	print(parsed)
 	return parsed
 
+def get_face_data_img(img):
+	headers = {
+    	'Content-Type': 'application/octet-stream',
+     	'Ocp-Apim-Subscription-Key': KEY,
+	}
+	params = {
+    	'returnFaceId': 'true',
+	}
+	path_to_face_api = '/detect'
+	response = requests.post(BASE_URL + path_to_face_api,
+    	                     data=img, 
+        	                 headers=headers,
+            	             params=params)
+	parsed = response.json()
+	print(parsed)
+	return parsed
+
+def show_webcam():
+    cam = cv2.VideoCapture(0)
+    ret_val, img = cam.read()
+    _, img = cv2.imencode('.jpg', img)
+    cv2.imwrite('test.jpg', img)
+    return img.tostring()
 
 #################################################
 filename1 = '/home/louiefu/Desktop/makentu/photo_Make/Joe/29831505_1779717248774811_2052102016_o.jpg'
 filename2 = '/home/louiefu/Desktop/makentu/photo_Make/Cynthia/29829371_1779717112108158_1600889678_o.jpg'
 filename3 = '/home/louiefu/Desktop/makentu/photo_Make/Louis/29831508_1160033747472618_1812921281_o.jpg'
 filename4 = '/home/louiefu/Desktop/makentu/photo_Make/Pierre/29893563_1724619870894340_1212997725_o.jpg'
-test_file = '/home/louiefu/Desktop/makentu/photo_Make/Pierre/29893563_1724619870894340_1212997725_o.jpg'
+#test_file = '/home/louiefu/Desktop/makentu/photo_Make/Pierre/29893563_1724619870894340_1212997725_o.jpg'
 
 files = [filename1, filename2, filename3, filename4]
 
 results = []
 for f in files:
-  r = get_face_data(f)
-  results.append(r)
+    r = get_face_data(f)
+    results.append(r)
 
 all_faceid = [f['faceId'] for image in results for f in image]
 
-#test_url = 'http://img.wennermedia.com/article-leads-vertical-300/1250530894_brad_pitt_290x402.jpg'
-#test_result = CF.face.detect(test_url)
-#test_faceId = test_result[0]['faceId']
-
-test_result = get_face_data(test_file)
+#test_result = get_face_data(test_file)
+test_img = show_webcam()
+test_result = get_face_data_img(test_img)
 test_faceId = test_result[0]['faceId']
 
 for f in all_faceid:
-  r = CF.face.verify(f, test_faceId)
-  print(r)
+    r = CF.face.verify(f, test_faceId)
+    print(r)
 
 
 # You can use this example JPG or replace the URL below with your own URL to a JPEG image.
